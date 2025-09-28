ed028f11fbbe6e92cf965f6a21f12863276a86e2
mbachel <mattbachscouts@gmail.com>
2025-09-27 23:10:45 -0400

    feat: implemented minihero

--- src/components/MiniHero.tsx
+++ src/components/MiniHero.tsx
@@ -1,7 +1,53 @@
+import { FaLinkedin, FaGithub } from "react-icons/fa";
+import { MdMail } from "react-icons/md";
+
+import Image from "next/image";
+import Link from "next/link";
+
 export default function MiniHero() {
     return (
-        <div>
-            Mini Hero works!
-        </div>
+        <header className="flex flex-col font-inter items-center justify-center h-fit text-center pt-16">
+            <div className="pb-4 mb-2">
+                <Image 
+                    src="/me.png"
+                    alt="Profile Picture" 
+                    width={148} 
+                    height={148} 
+                    className="w-60 h-60 rounded-full object-cover object-center items-center mx-auto"
+                    id="profile-picture"
+                    priority
+                />
+            </div>
+            <div className="pb-6 mt-auto">
+                <Link 
+                    href="/contact" 
+                    title="Contact Me">
+                    <MdMail
+                        size={32}
+                        className="inline-block mx-2"
+                    />
+                </Link>
+                <Link 
+                    href="https://www.linkedin.com/in/matthewbachelder/" 
+                    title="LinkedIn" 
+                    rel="noopener noreferrer" 
+                    target="_blank">
+                    <FaLinkedin
+                        size={32}
+                        className="inline-block mx-2"
+                    />
+                </Link>
+                <Link 
+                    href="https://github.com/mbachel" 
+                    title="GitHub" 
+                    rel="noopener noreferrer" 
+                    target="_blank">
+                    <FaGithub
+                        size={32}
+                        className="inline-block mx-2"
+                    />
+                </Link>
+            </div>
+        </header>
     );
 }
