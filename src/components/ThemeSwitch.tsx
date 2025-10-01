0930393879cb5042f655a12f168bf158023807a5
mbachel <mattbachscouts@gmail.com>
2025-09-30 20:00:52 -0400

    fix: fixing bugs

--- src/components/ThemeSwitch.tsx
+++ src/components/ThemeSwitch.tsx
@@ -14,8 +14,8 @@ export default function ThemeSwitch() {
     if (!mounted) return (
         <Image 
             src="data:image/svg+xml;base64,PHN2ZyBzdHJva2U9IiNGRkZGRkYiIGZpbGw9IiNGRkZGRkYiIHN0cm9rZS13aWR0aD0iMCIgdmlld0JveD0iMCAwIDI0IDI0IiBoZWlnaHQ9IjIwMHB4IiB3aWR0aD0iMjAwcHgiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHJlY3Qgd2lkdGg9IjIwIiBoZWlnaHQ9IjIwIiB4PSIyIiB5PSIyIiBmaWxsPSJub25lIiBzdHJva2Utd2lkdGg9IjIiIHJ4PSIyIj48L3JlY3Q+PC9zdmc+Cg=="
-            width={32}
-            height={32}
+            width={40}
+            height={40}
             sizes="36x36"
             alt="Loading theme toggle"
             priority={false}
